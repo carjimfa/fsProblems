f164f71340b4f578eac099b29add98999dc53db3
Carlos Jimenez <carjimfa@gmail.com>
2018-11-04 20:19:43 +0100

    Adds Math Package and leap year problem

---
+++ fsProblems.DataStructures/find-duplicated.fsx
@@ -0,0 +1,6 @@
+﻿let sourceArray = [1;2;3;4;4;5;6;7]
+let sourceArraySorted = List.sort sourceArray
+
+let findDuplicates sourceArray =
+    List.sort sourceArray
+
---
+++ fsProblems.Math/leap-year.fsx
@@ -0,0 +1,11 @@
+﻿
+let leapYear (year: int): bool = 
+    match (year%4) with 
+    | 0 -> 
+        match (year%100) with
+        | 0 ->
+            match (year % 400) with
+            | 0 -> true
+            | x -> false
+        | x-> true
+    | x -> false
