e23a986c4785e6280ee831322e4de0a4d1ef7153
Carlos Jimenez <carjimfa@gmail.com>
2018-11-04 19:36:22 +0100

    First Data Structure problems - FindMaximum and sumOfSquares

---
+++ fsProblems.DataStructures/find-maximum.fsx
@@ -0,0 +1,12 @@
+﻿let sourceArray = [1;2;3;4;5;6;7;8;9;1;23;5;3;1]
+
+let rec findMaximum2 sourceArray (max:int) =
+    match sourceArray with
+    | []-> max
+    | first::rest -> if first > max then findMaximum2 rest first else findMaximum2 rest max
+
+
+let findMaximum sourceArray=
+    findMaximum2 sourceArray 0
+
+findMaximum sourceArray
---
+++ fsProblems.DataStructures/sum-of-squares.fsx
@@ -0,0 +1,8 @@
+﻿let sourceArray=[1;2;3;4;5;6;7;8]
+
+let rec sumOfSquares sourceArray=
+    match sourceArray with
+    | []->0
+    | x::xs -> (x*x) + sumOfSquares xs
+
+sumOfSquares sourceArray
